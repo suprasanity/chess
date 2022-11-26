aa80ac846dbf5919c98bd494b261e710c2e5d1d4
pascal <dupascal00@gmail.com>
2022-11-26 22:06:41 +0100

    Add legalMove For P

--- src/main/java/Piece/Pawn.java
+++ src/main/java/Piece/Pawn.java
@@ -2,20 +2,23 @@ package Piece;
 
 
 import jeu.Board;
+import jeu.Move;
 import jeu.Square;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Pawn implements Piece {
     private String color;
     private final int VALUE = 1;
     private char symbol;
     private boolean firstMove;
-    public void setColor(String color,Boolean firstMove){
+    public void setColor(String color){
         this.color=color;
-        this.firstMove = firstMove;
+      
         setSymbol(color);
     }
     public void setSymbol(String color){
@@ -27,18 +30,58 @@ public class Pawn implements Piece {
     public String getColor(){
         return this.color;
     }
+    
     public int getValue(){
         return this.VALUE;
     }
     public boolean isFirstMove(){
         return this.firstMove;
     }
-    public List<Square> legalMovSquares(Square square, List<Square> Board){
-        if(this.color.equals("Black")){
-
-        }
+    public List<Move> legalMovSquares(Square square){
+        List<Move> legalMove = new ArrayList<>();
+        int index =0;
+        int[] listDirection;
+        if(square.getPiece() instanceof Pawn){
+            //case if row 7 or row 2
+            for(Square findSquare : Board.lesCase){// en trop ? mettre getid pour chaque case ?
+                if(square.equals(findSquare)){
+                    index = Board.lesCase.indexOf(findSquare);
+                }
+            }
+            // pawn attack piece each side if exist
             
-        
+            listDirection = new int[]{-7,-9};
+            for(int direction : listDirection){
+                int nextPossibleMove = direction + index;
+                if(Board.lesCase.get(nextPossibleMove).getPiece() != null
+                    && Objects.equals(Board.lesCase.get(nextPossibleMove).getPiece().getColor(),
+                                      Board.p.getOpponnentColor()))
+                {
+                    legalMove.add(new Move(nextPossibleMove, index, new Pawn()));
+                }
+            }
+            // pawn just move
+            if(square.getNumber() == 2 && Board.p.getCurrentColor().equals("White") ||
+               square.getNumber()== 7 && Board.p.getCurrentColor().equals("Black")){
+                listDirection = new int[]{-8,-16};
+                for(int direction : listDirection){
+                    int nextPossibleSquare = direction + index;
+                    if(Board.lesCase.get(nextPossibleSquare).getPiece() == null){
+                        legalMove.add(new Move(nextPossibleSquare,index,new Pawn()));
+                    }
+                }
+            }
+            else{
+                int direction = -8;
+                int nextPossibleSquare = direction + index;
+                if(nextPossibleSquare>=Board.START_INDEX_BOARD){
+                    if(Board.lesCase.get(nextPossibleSquare).getPiece() == null){
+                        legalMove.add(new Move(nextPossibleSquare, index, new Pawn()));
+                    }
+                }
+            } 
+        }
+        return legalMove;     
     }
     @Override
     public String toString() {
